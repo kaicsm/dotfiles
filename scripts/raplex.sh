fb3b16900406ca5adca50a21186cdbc7496aa5d6
Kaic Salomão <68879185+kaicsm@users.noreply.github.com>
2023-07-25 16:44:00 -0300

    default

--- scripts/raplex.sh
+++ scripts/raplex.sh
@@ -3,8 +3,9 @@
 ra-verify > /dev/null
 exit_code=$?
 
-# verifica se a porta está iniciada
+# verifica se a porta não está iniciada
 if [ $exit_code -eq 0 ]; then
+  # se estiver:
   # iniciar o servidor
   ra-multiplex server &
 fi
